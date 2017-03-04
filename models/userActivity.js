e29e3de3112adbeff9a5cb868de6bf4311543ab7
olga_deineko <olga_deineko@ukr.net>
2017-03-05 00:10:51 +0200

    init

--- models/userActivity.js
+++ models/userActivity.js
@@ -6,7 +6,9 @@ class User {
   static *getAllUserActivity() {
     return  yield knex.select('*').from('useractivity');
   }
-
+  static *getAllType() {
+    return  yield knex.select().from('product_type');
+  }
 
 }
 module.exports = User;
